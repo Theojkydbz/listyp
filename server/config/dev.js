24a89e372a0c15e98ddc5bc4fb78524c4eb52a5c
Théo GEILLER <42747893+Theojkydbz@users.noreply.github.com>
2023-01-25 09:35:16 -0500

    Delete dev.js

--- server/config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    DB_URI: 'mongodb+srv://theotheo:sIr2C82mq8MVzB5H@cluster0.ymdzwhs.mongodb.net/?retryWrites=true&w=majority',
-    JWT_SECRET: 'sdfa7ew8Ksdqwa',
-    SESSION_SECRET:'putanythinghere'
-}
